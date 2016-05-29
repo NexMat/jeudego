047e52dad410ecee947e4b84f1ee8ca3cddf6c02
NexMat <vu.mathieu@gmail.com>
2016-05-29 23:49:53 +0200

    ia lvl1

--- IA_level1.py
+++ IA_level1.py
@@ -41,37 +41,62 @@ class IA_level1(Joueur):
         -> si oui, combien de cases ?
         -> suicide ? etc ..
         """
+        # je met la valeur de taille en memoire (gain de complexité)
         n = self.game.goban.taille
+        # j'initialise des valeurs de lignes et colonnes
         lgn, col = 0, 0
+        # j'initialise mon importance maximale
         imp_tmp  = 0
+        # num correspond à la solution choisi pour le debut de partie (cf Quality -> Fuseki (num) )
         num = 0
+        # je calcule ma liste des coups de debut de partie
         L = self.quality.fuseki(num)
         Liste = []
-        coord_none = [(i, j) for i in range(n) for j in range(n) if self.game.goban.cell[i][j] == None]
         
         # Debut de partie
+        # tant que L n'est pas vide , si le coup est jouable, je le joue
         for i in range (len(L)) :
             try:
                 if self.game.goban.test_move(L[i][0],L[i][1],self)==False:
                     return L[i][0],L[i][1]
             except:
                 pass
-        
-            
-        for (i,j) in coord_none :
-            importance = self.quality.importance(i, j)
-            if (importance > imp_tmp):
-                Liste = [(i,j)]
-                imp_tmp  = importance
-            if (importance == imp_tmp):
-                Liste+=[(i,j)]
-                
-        N = len(Liste)
-        k = random.randint(1,N-1)
 
-        if imp_tmp == 0 :
-            for col in range(n):
-                for lgn in range(n):
+        # je cree une liste des coups non joués
+        coord_none = [(i, j) for i in range(n) for j in range(n) if self.game.goban.cell[i][j] == None]
+        coup = "pass"
+        
+        # sinon, je parcours l'ensemble des coups non joués   
+        for (i,j) in coord_none :
+            try: 
+                if self.game.goban.test_move(j, i, self) == False:
+                    importance = self.quality.importance(j, i)
+                    if (importance > imp_tmp): # si le coup est le plus important on le choisit seul
+                        coup =(i,j)
+                        imp_tmp  = importance
+            except Forbidden_move as e:
+                pass
+
+        
+        if imp_tmp != 0 :
+            return coup
+        
+        #si aucun coup n'est jouable je lance le programme IA_random
+        else :
+            coord = "pass"
+            for i in range(10000):                #on teste aléatoirement 10000 fois 
+                col = random.randint(0, self.game.goban.taille - 1)
+                lgn = random.randint(0, self.game.goban.taille - 1)
+                try: 
+                    if self.game.goban.test_move(col, lgn, self) == False:
+                        coord = (col, lgn)
+                        sys.stdout.flush()
+                        return coord
+                except Forbidden_move as e:
+                    pass
+
+            for col in range(self.game.goban.taille):
+                for lgn in range(self.game.goban.taille):
                     try: 
                         if not self.game.goban.test_move(col, lgn, self) == False:
                             coord = (col, lgn)
@@ -79,6 +104,12 @@ class IA_level1(Joueur):
                     except:
                         pass
 
-        return(Liste[k][0],Liste[k][1])
+            return coord
+            
+
+       
+                     
+        
+        
         
     
